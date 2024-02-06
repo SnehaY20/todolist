6a670f9b02fbde0dd5861dfb83799ad95119a80d
Sneha Yadav <105566600+SnehaY20@users.noreply.github.com>
2024-02-06 15:46:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.get("/", function(req, res){
+  
+   
+ 
+ res.render("list", {
+  listTitle: day, newListItems: items
+ });
+ 
+});
+app.post("/", function(req, res){
+
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+  
+});
+app.get("/work", function(req,res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+app.get("/about", function(req, res){
+  res.render("about");
+});
+app.listen(PORT, function(){
+  console.log("Server started on port 3000.");
+});
